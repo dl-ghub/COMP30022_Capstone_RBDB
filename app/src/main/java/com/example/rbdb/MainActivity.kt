0a3e90d8f9aadd579f16f5b2ba3267e10fcfdc32
pakhow <pakhow@student.unimelb.edu.au>
2021-09-07 00:10:25 +0800

    Added template for header (toolbar) and search button

--- app/src/main/java/com/example/rbdb/MainActivity.kt
+++ app/src/main/java/com/example/rbdb/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.rbdb
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.SearchView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        val menuItem = menu!!.findItem(R.id.search)
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                Toast.makeText(this@MainActivity, "Search is expanded", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                Toast.makeText(this@MainActivity, "Search is collapsed", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
+        val searchItem = menu!!.findItem(R.id.search)
+        var searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
+        searchView.queryHint = "Search here..."
+
+        return true
     }
 }
