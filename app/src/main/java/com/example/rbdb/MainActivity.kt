b95147ee80f37690a002b5e1672ae6c25ba87bcf
pakhow <pakhow@student.unimelb.edu.au>
2021-09-22 18:14:53 +0800

    Fixed bug where apps cannot launch
    Added things that were removed due to merging Kritnand's branch

--- app/src/main/java/com/example/rbdb/MainActivity.kt
+++ app/src/main/java/com/example/rbdb/MainActivity.kt
@@ -6,27 +6,93 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
-import android.widget.Button
-import android.content.Intent
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.topAppBar)
+        //setSupportActionBar(toolbar)
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // Handle search icon press
+                    Toast.makeText(this, "search bar pressed", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    Toast.makeText(this, "more button pressed", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        val tabLayout = findViewById<TabLayout>(R.id.TabLayout)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
 
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
 
-        var button = findViewById<View>(R.id.addBtn) as Button
-        button.setOnClickListener(View.OnClickListener { openNewActivity() })
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
+
+        class FragmentAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity){
+            override fun getItemCount(): Int {
+
+                return 3
+            }
+
+            override fun createFragment(position: Int): Fragment {
+
+                return when(position){
+                    0->ContactFragment.newInstance()
+                    1->GroupFragment.newInstance()
+                    2->TagFragment.newInstance()
+                    else -> ContactFragment.newInstance()
+                }
+            }
+
+        }
+        viewPager.adapter = FragmentAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "contact"
+
+                }
+                1 -> {
+                    tab.text = "group"
+
+                }
+                2->{
+                    tab.text = "tags"
+                }
+
+            }
+        }.attach()
     }
 
-<<<<<<< Updated upstream
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
-
         val menuItem = menu!!.findItem(R.id.search)
         menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
             override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
@@ -38,40 +104,9 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
-
         val searchItem = menu!!.findItem(R.id.search)
         var searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
         searchView.queryHint = "Search here..."
-
         return true
-    }
-
-    fun openNewActivity() {
-        val intent = Intent(this, AddContactActivity::class.java)
-        startActivity(intent)
-    }
-=======
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        val inflater = menuInflater
-//        inflater.inflate(R.menu.menu, menu)
-//
-//        val menuItem = menu!!.findItem(R.id.search)
-//        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-//            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-//                Toast.makeText(this@MainActivity, "Search is expanded", Toast.LENGTH_SHORT).show()
-//                return true
-//            }
-//            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-//                Toast.makeText(this@MainActivity, "Search is collapsed", Toast.LENGTH_SHORT).show()
-//                return true
-//            }
-//        })
-//
-//        val searchItem = menu!!.findItem(R.id.search)
-//        var searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
-//        searchView.queryHint = "Search here..."
-//
-//        return true
-//    }
->>>>>>> Stashed changes
+    }*/
 }
