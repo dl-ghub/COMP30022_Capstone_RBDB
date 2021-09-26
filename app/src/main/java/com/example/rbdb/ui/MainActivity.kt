14df38d0808b3104da765c37a84996ea943f2a95
ksuwannaarj <87976073+ksuwannaarj@users.noreply.github.com>
2021-09-26 15:11:06 +0700

    fixed merge issues

--- app/src/main/java/com/example/rbdb/ui/MainActivity.kt
+++ app/src/main/java/com/example/rbdb/ui/MainActivity.kt
@@ -1,30 +1,91 @@
 package com.example.rbdb.ui
 
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import com.example.rbdb.database.AppDatabase
+import androidx.navigation.NavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.rbdb.R
+import com.example.rbdb.ui.adapters.FragmentAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
-
-
-    // VV DEFAULT CODE BY IDE VV
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.topAppBar)
         setSupportActionBar(toolbar)
+
+        val tabLayout = findViewById<TabLayout>(R.id.TabLayout)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
+
+
+        viewPager.adapter = FragmentAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "contact"
+
+                }
+                1 -> {
+                    tab.text = "group"
+
+                }
+                2->{
+                    tab.text = "tags"
+                }
+
+            }
+        }.attach()
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.search -> {
+            Toast.makeText(this, "search bar pressed", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.more -> {
+            Toast.makeText(this, "more button pressed", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
 
@@ -45,5 +106,5 @@ class MainActivity : AppCompatActivity() {
         searchView.queryHint = "Search here..."
 
         return true
-    }
+    }*/
 }
