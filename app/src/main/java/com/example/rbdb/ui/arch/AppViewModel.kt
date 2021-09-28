43ee3647ff3bc45f5281d5d3a9048bdd54f1a16e
Xomchua <andersen.mattias@gmail.com>
2021-09-28 15:09:37 +0800

    Added threads for database calls

--- app/src/main/java/com/example/rbdb/ui/arch/AppViewModel.kt
+++ app/src/main/java/com/example/rbdb/ui/arch/AppViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.rbdb.ui.arch
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.rbdb.database.AppDatabase
 import com.example.rbdb.database.model.*
+import kotlinx.coroutines.launch
 
-class AppViewModel(): ViewModel() {
+class AppViewModel: ViewModel() {
 
     private lateinit var repository: AppRepository
 
@@ -13,48 +17,84 @@ class AppViewModel(): ViewModel() {
     }
 
     // Call repository methods for card
-    fun insertCard(cardEntity: CardEntity){repository.insertCard(cardEntity)}
+    fun insertCard(cardEntity: CardEntity){viewModelScope.launch {repository.insertCard(cardEntity)}}
 
-    fun deleteCard(cardEntity: CardEntity){repository.deleteCard(cardEntity)}
+    fun deleteCard(cardEntity: CardEntity){viewModelScope.launch {repository.deleteCard(cardEntity)}}
 
-    fun updateCard(cardEntity: CardEntity){repository.updateCard(cardEntity)}
+    fun updateCard(cardEntity: CardEntity){viewModelScope.launch {repository.updateCard(cardEntity)}}
 
-    fun getAllCards(): List<CardEntity>{return repository.getAllCards()}
+    fun getAllCards(): LiveData< List<CardEntity> > {
+        val result = MutableLiveData< List<CardEntity> >()
+        viewModelScope.launch {result.postValue(repository.getAllCards())}
+        return result
+    }
 
-    fun getCardWithTags(): List<CardWithTagsEntity>{return repository.getCardWithTags()}
+    fun getCardWithTags(): LiveData< List<CardWithTagsEntity> >{
+        val result = MutableLiveData< List<CardWithTagsEntity> >()
+        viewModelScope.launch {result.postValue(repository.getCardWithTags())}
+        return result
+    }
 
-    fun getCardWithLists(): List<CardWithListsEntity>{return repository.getCardWithLists()}
+    fun getCardWithLists(): LiveData< List<CardWithListsEntity> >{
+        val result = MutableLiveData< List<CardWithListsEntity> >()
+        viewModelScope.launch {result.postValue(repository.getCardWithLists())}
+        return result
+    }
 
     // Call repository methods for list
-    fun insertList(listEntity: ListEntity){repository.insertList(listEntity)}
+    fun insertList(listEntity: ListEntity){viewModelScope.launch {repository.insertList(listEntity)}}
 
-    fun deleteList(listEntity: ListEntity){repository.deleteList(listEntity)}
+    fun deleteList(listEntity: ListEntity){viewModelScope.launch {repository.deleteList(listEntity)}}
 
-    fun updateList(listEntity: ListEntity){repository.updateList(listEntity)}
+    fun updateList(listEntity: ListEntity){viewModelScope.launch {repository.updateList(listEntity)}}
 
-    fun getAllLists(): List<ListEntity>{return repository.getAllLists()}
+    fun getAllLists(): LiveData< List<ListEntity> >{
+        val result = MutableLiveData< List<ListEntity> >()
+        viewModelScope.launch {result.postValue(repository.getAllLists())}
+        return result
+    }
 
-    fun getListWithCards(): List<ListWithCardsEntity>{return repository.getListWithCards()}
+    fun getListWithCards(): LiveData< List<ListWithCardsEntity> >{
+        val result = MutableLiveData< List<ListWithCardsEntity> >()
+        viewModelScope.launch {result.postValue(repository.getListWithCards())}
+        return result
+    }
 
     // Call repository methods for tag
-    fun insertTag(tagEntity: TagEntity){repository.insertTag(tagEntity)}
+    fun insertTag(tagEntity: TagEntity){viewModelScope.launch {repository.insertTag(tagEntity)}}
 
-    fun deleteTag(tagEntity: TagEntity){repository.deleteTag(tagEntity)}
+    fun deleteTag(tagEntity: TagEntity){viewModelScope.launch {repository.deleteTag(tagEntity)}}
 
-    fun updateTag(tagEntity: TagEntity){repository.updateTag(tagEntity)}
+    fun updateTag(tagEntity: TagEntity){viewModelScope.launch {repository.updateTag(tagEntity)}}
 
-    fun getAllTags(): List<TagEntity>{return repository.getAllTags()}
+    fun getAllTags(): LiveData< List<TagEntity> >{
+        val result = MutableLiveData< List<TagEntity> >()
+        viewModelScope.launch {result.postValue(repository.getAllTags())}
+        return result
+    }
 
-    fun getTagWithCards(): List<TagWithCardsEntity>{return repository.getTagWithCards()}
+    fun getTagWithCards(): LiveData < List<TagWithCardsEntity> >{
+        val result = MutableLiveData< List<TagWithCardsEntity> >()
+        viewModelScope.launch {result.postValue(repository.getTagWithCards())}
+        return result
+    }
 
-    fun getTagID(nameOfTag: String): Long{return repository.getTagID(nameOfTag)}
+    fun getTagID(nameOfTag: String): LiveData< Long >{
+        val result = MutableLiveData< Long >()
+        viewModelScope.launch {result.postValue(repository.getTagID(nameOfTag))}
+        return result
+    }
 
     // Call repository methods for user
-    fun insertUser(userEntity : UserEntity){repository.insertUser(userEntity)}
+    fun insertUser(userEntity : UserEntity){viewModelScope.launch {repository.insertUser(userEntity)}}
 
-    fun deleteUser(userEntity : UserEntity){repository.deleteUser(userEntity)}
+    fun deleteUser(userEntity : UserEntity){viewModelScope.launch {repository.deleteUser(userEntity)}}
 
-    fun updateUser(userEntity : UserEntity){repository.updateUser(userEntity)}
+    fun updateUser(userEntity : UserEntity){viewModelScope.launch {repository.updateUser(userEntity)}}
 
-    fun getAllUsers(): List<UserEntity>{return repository.getAllUsers()}
+    fun getAllUsers(): LiveData< List<UserEntity> >{
+        val result = MutableLiveData< List<UserEntity> >()
+        viewModelScope.launch {result.postValue(repository.getAllUsers())}
+        return result
+    }
 }
