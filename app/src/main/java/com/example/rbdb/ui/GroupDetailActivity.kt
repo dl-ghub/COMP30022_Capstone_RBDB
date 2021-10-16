a35b92b51728d00ee84c539ff69132763dd6a239
pakhow <pakhow@student.unimelb.edu.au>
2021-10-16 16:13:26 +0800

    Changed button layout design in group details page
    Functionality is yet to be implemented

--- app/src/main/java/com/example/rbdb/ui/GroupDetailActivity.kt
+++ app/src/main/java/com/example/rbdb/ui/GroupDetailActivity.kt
@@ -1,18 +1,25 @@
 package com.example.rbdb.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rbdb.R
 import com.example.rbdb.databinding.ActivityGroupBinding
+import com.example.rbdb.ui.arch.AppViewModel
 
 class GroupDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGroupBinding
+    private val viewModel: AppViewModel by viewModels()
+    private var groupId: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGroupBinding.inflate(layoutInflater)
@@ -32,7 +39,7 @@ class GroupDetailActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
+        inflater.inflate(R.menu.activity_group_menu, menu)
         return true
     }
 
@@ -42,12 +49,7 @@ class GroupDetailActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.search -> {
-            Toast.makeText(this, "search pressed", Toast.LENGTH_SHORT).show()
-            true
-        }
-
-        R.id.more -> {
+        R.id.edit -> {
             val builder = AlertDialog.Builder(this)
             builder.setMessage(R.string.new_group_name)
             val inflater = layoutInflater.inflate(R.layout.view_holder_new_dialog, null)
@@ -63,11 +65,43 @@ class GroupDetailActivity : AppCompatActivity() {
             alertDialog.show()
             true
         }
+        R.id.search -> {
+            Toast.makeText(this, "search pressed", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.delete -> {
+            Toast.makeText(this, "delete pressed", Toast.LENGTH_SHORT).show()
+            true
+// add this in when the delete button query for the database is done
+//            val builder = AlertDialog.Builder(this)
+//            builder.setMessage(R.string.confirm_delete_group)
+//            builder.setPositiveButton("Delete") { _, _ ->
+//                deleteGroup(groupId)
+//
+//                // Return to Homepage
+//                val intent = Intent(this, MainActivity::class.java)
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+//                startActivity(intent)
+//
+//                finish()
+//            }
+//
+//            builder.setNegativeButton("Cancel") { _, _ -> }
+//
+//            val alertDialog: AlertDialog = builder.create()
+//            alertDialog.show()
+//            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+//                .setTextColor(resources.getColor(R.color.warningRed))
+//
+//            true
+        }
         else -> {
-            // If we got here, the user's action was not recognized.
-            // Invoke the superclass to handle it.
             super.onOptionsItemSelected(item)
         }
     }
 
+//    private fun deleteGroup(groupId: Long) {
+//        Log.d("groupId to be deleted", groupId.toString())
+//        viewModel.deleteCardAndCrossRefByCardId(groupId)
+//    }
 }
